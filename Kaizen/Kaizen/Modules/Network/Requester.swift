ce1d0816f479e0dd727228b8a21c6d13c474fe58
TheusMartins <ml_martins@yahoo.com>
2024-05-18 16:24:51 -0300

    Creted Requester

---
+++ Kaizen/Kaizen/Modules/Network/Requester.swift
@@ -0,0 +1,55 @@
+//
+//  Requester.swift
+//  Kaizen
+//
+//  Created by Scizor on 5/18/24.
+//
+
+import Foundation
+
+protocol Requester {
+    func request(basedOn infos: RequestInfos) async throws -> RequestSuccessResponse
+}
+
+final class DefaultRequester: Requester {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    func request(basedOn infos: RequestInfos) async throws -> RequestSuccessResponse {
+        guard let request = buildURLRequest(basedOn: infos) else {
+            throw BaseRequestErrors.invalidURL
+        }
+        
+        do {
+            let (data, response) = try await session.data(for: request)
+            return .init(data: data, response: response)
+        } catch {
+            throw BaseRequestErrors.invalidResponse
+        }
+    }
+    
+    // MARK: Helpers
+    
+    private func buildURLRequest(basedOn infos: RequestInfos) -> URLRequest? {
+        guard let baseURL = infos.baseURL, var url = URLComponents(string: "\(baseURL)\(infos.endpoint)") else {
+            return nil
+        }
+        
+        var components: [URLQueryItem] = []
+        
+        infos.parameters?.forEach({ key, value in
+            components.append(URLQueryItem(name: key, value: "\(value)"))
+        })
+        
+        url.queryItems = components
+        
+        guard let finalURL = url.url else {
+            return nil
+        }
+        
+        return URLRequest(url: finalURL)
+    }
+}
