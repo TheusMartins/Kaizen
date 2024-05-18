25924878fb843e6757d2c6c99aa5c92f5eb169ec
TheusMartins <ml_martins@yahoo.com>
2024-05-18 16:43:41 -0300

    Implemented BetListingRepository

--- Kaizen/Kaizen/Modules/BetListing/BetListingRepository.swift
+++ Kaizen/Kaizen/Modules/BetListing/BetListingRepository.swift
@@ -6,4 +6,39 @@
 //
 
 import Foundation
+import Network
+
+protocol BetListingRepository {
+    func getBets() async throws -> [BetListingModel]
+}
+
+final class BetListingRepositoryImplementation: BetListingRepository {
+    // MARK: - Private properties
+    
+    private var requester: Requester
+    
+    // MARK: - Initialization
+    
+    init(requester: Requester = DefaultRequester()) {
+        self.requester = requester
+    }
+    
+    // MARK: - Open methods
+    
+    func getBets() async throws -> [BetListingModel] {
+        let response = try await requester.request(basedOn: BetListingRequest.getBets)
+        return try await parseBetsResponse(response: response)
+    }
+    
+    // MARK: - Private methods
+    
+    private func parseBetsResponse(response: RequestSuccessResponse) async throws -> [BetListingModel] {
+        do {
+            let model = try JSONDecoder().decode([BetListingModel].self, from: response.data)
+            return model
+        } catch {
+            throw error
+        }
+    }
+}
 
